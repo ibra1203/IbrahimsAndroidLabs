ef7e05678eed588ddffe9ffae42e9afd24da3b91
imahf <gghgh>
2022-09-14 12:48:58 -0400

    Initial Commit

---
+++ app/src/main/java/algonquin/cst2335/mahf0040/MainActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.mahf0040;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
